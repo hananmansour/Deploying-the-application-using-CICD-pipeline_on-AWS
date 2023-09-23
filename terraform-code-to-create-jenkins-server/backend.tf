04a1b7e26a53eebf49f2981db74419552e9aa25e
Hanan Mansour <66485038+hananmansour@users.noreply.github.com>
2023-09-24 00:05:44 +0300

    Delete terraform-code-to-create-jenkins-server directory

--- terraform-code-to-create-jenkins-server/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "nginx-app"
-    region = "us-east-1"
-    key = "jenkins-server/terraform.tfstate"
-  }
-}
--- terraform-code-to-create-jenkins-server/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
