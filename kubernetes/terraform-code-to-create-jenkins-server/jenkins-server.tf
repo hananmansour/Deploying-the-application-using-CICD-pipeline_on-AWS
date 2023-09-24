9a1fbbbb63a0866138d922a1ab6de33ebae9a1ef
Hanan Mansour <66485038+hananmansour@users.noreply.github.com>
2023-09-24 16:12:15 +0300

    Add files via upload

---
+++ kubernetes/terraform-code-to-create-jenkins-server/jenkins-server.tf
@@ -0,0 +1,47 @@
+
+resource "aws_instance" "myapp-server" {
+  ami                         = "ami-0d406e26e5ad4de53"
+  instance_type               = var.instance_type
+  key_name                    = "first-key"
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
+  availability_zone           = var.avail_zone
+  associate_public_ip_address = true
+
+  user_data = <<-EOL
+ 
+	#!/bin/bash
+
+	# install jenkins
+
+	sudo yum update
+	sudo wget -O /etc/yum.repos.d/jenkins.repo \
+	    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+	sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+	sudo yum upgrade -y
+	sudo amazon-linux-extras install java-openjdk11 -y
+	sudo yum install jenkins -y
+	sudo systemctl enable jenkins
+	sudo systemctl start jenkins
+
+	# install git
+	sudo yum install git -y
+
+	# install terraform
+
+	sudo yum install -y yum-utils
+	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+	sudo yum -y install terraform
+
+	# install kubectl
+
+	sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+	sudo chmod +x ./kubectl
+	sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+	                                         
+  EOL
+  
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
