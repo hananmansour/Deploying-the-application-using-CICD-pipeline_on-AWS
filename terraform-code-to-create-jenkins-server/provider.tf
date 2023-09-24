e347cdf75c1b660ff81e4667ff8d84b3256e82cd
Hanan Mansour <66485038+hananmansour@users.noreply.github.com>
2023-09-24 16:14:08 +0300

    Add files via upload

---
+++ terraform-code-to-create-jenkins-server/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "AKIAV7ZGHJVSA4SHUH6N"
+  secret_key = "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"
+  region = "us-east-2"
+}
+
