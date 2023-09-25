af0e0b04b64c5dcc36ee82a16a914015e2e0a372
Hanan Mansour <66485038+hananmansour@users.noreply.github.com>
2023-09-26 02:00:48 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,4 +5,16 @@ variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-east-2"
+
+}
+variable "subnet_ids" {
+    type = list
+  
+}
+variable "security_group" {
+  type = string
+}
+variable "all-subnet_ids" {
+    type = list
+  
 }
