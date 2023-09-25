ff445fa9089a29a5fcacaa9c9f28972fe466403b
Hanan Mansour <66485038+hananmansour@users.noreply.github.com>
2023-09-26 00:46:16 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,8 +69,7 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
-      instance_types = ["t3.small"]
+       instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 2
